8cfdba91e3d95ec58fcf22bc19b453a55fc209bf
jaissica <jaissica.hora@gmail.com>
2023-05-27 13:44:44 -0400

    Implementing Single Page Applications

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+function App() {
+  return (
+      <h1>Hello World!</h1>
+  );
+}
+export default App;
