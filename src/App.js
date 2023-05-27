8b36be1cc0fdac2e7ac9dd7c24d133f53217f9c6
jaissica <jaissica.hora@gmail.com>
2023-05-27 14:12:52 -0400

    Creating a Tuiter placeholder component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Labs from "./labs";
 import HelloWorld from "./labs/a3/hello-world";
+import Tuiter from "./tuiter";
 function App() {
    return (
       <div className="container">
          <HelloWorld/>
          <Labs/>
+         <Tuiter/>
       </div>
    );
 }
