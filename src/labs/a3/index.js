8574acc62c77cf8c02216fb4cddfc98df4c70314
jaissica <jaissica.hora@gmail.com>
2023-05-27 14:14:19 -0400

    Learning about JavaScript

--- src/labs/a3/index.js
+++ src/labs/a3/index.js
@@ -1,7 +1,9 @@
+import JavaScript from "./java-script";
 function Assignment3() {
     return (
       <div>
         <h1>Assignment 3</h1>
+        <JavaScript/>
       </div>
     );
    }
---
+++ src/labs/a3/java-script.js
@@ -0,0 +1,8 @@
+function JavaScript() {
+    return(
+       <div>
+          <h1>JavaScript</h1>
+       </div>
+    );
+ }
+ export default JavaScript
