06fee20c12b3313a4d1c1caeb7b7e9331540176b
jaissica <jaissica.hora@gmail.com>
2023-05-27 15:22:13 -0400

    Variables and Constants

--- src/labs/a3/java-script.js
+++ src/labs/a3/java-script.js
@@ -1,7 +1,15 @@
+import VariablesAndConstants from "./variables-and-constants";
+import VariableTypes from "./variable-types";
+import BooleanVariable from "./boolean-variables";
+
 function JavaScript() {
+   console.log('Hello World!');
     return(
        <div>
           <h1>JavaScript</h1>
+          <VariablesAndConstants/>
+          <VariableTypes/>
+          <BooleanVariable/>
        </div>
     );
  }
