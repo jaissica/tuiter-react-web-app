74b75858b046dc3701595fd3aa5eadb1fe6dfc8e
jaissica <jaissica.hora@gmail.com>
2023-05-27 13:57:32 -0400

    Implementing the Labs component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Labs from "./labs";
 function App() {
-  return (
-      <h1>Hello World!</h1>
-  );
+   return (
+      <div className="container">
+         <h1>Hello World!</h1>
+         <Labs/>
+      </div>
+   );
 }
 export default App;
-
---
+++ src/labs/index.js
@@ -0,0 +1,8 @@
+function Labs() {
+    return(
+       <div>
+          <h1>Assignment 3</h1>
+       </div>
+    );
+ }
+ export default Labs;
