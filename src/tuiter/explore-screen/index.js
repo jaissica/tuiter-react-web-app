bd315742bfd3586092fd69923cf0031445610810
jaissica <jaissica.hora@gmail.com>
2023-05-27 21:37:10 -0400

    Final 2

--- src/tuiter/explore-screen/index.js
+++ src/tuiter/explore-screen/index.js
@@ -30,7 +30,7 @@ function ExploreScreen() {
        </li>
      </ul>
      <div className="position-relative mb-2">
-       <img src="/images/starship.jpg" className="w-100"/>
+       <img src="/images/starship_new.jpg" className="w-100"/>
        <h1 className="position-absolute wd-nudge-up text-white">
          SpaceX Starship</h1>
      </div>
