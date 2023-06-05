2a4585b6597dd9e718a3d9bbd2ad453e7ca36c9a
jaissica <jaissica.hora@gmail.com>
2023-06-05 19:28:24 -0400

    Lab

--- src/labs/a4/redux-examples/hello-redux-example-component.js
+++ src/labs/a4/redux-examples/hello-redux-example-component.js
@@ -2,6 +2,6 @@ import React from "react";
 import {useSelector} from "react-redux";
 const HelloReduxExampleComponent = () => {
  const message = useSelector((state) => state.hello.message);
- return(<h3>{message}</h3>);
+ return(<h1>{message}</h1>);
 };
 export default HelloReduxExampleComponent;
