c7e30239f9b1494cf570f6698f5f803cce3b0da7
jaissica <jaissica.hora@gmail.com>
2023-06-13 17:21:27 -0400

    Render Changes

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 // const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
 // const TUITS_API = `${SERVER_API_URL}/tuits`;
 const API_BASE = process.env.REACT_APP_SERVER_API_URL;
-const TUITS_API = `${API_BASE}/tuits`;
-// const TUITS_API = 'http://localhost:4000/api/tuits';
+// const TUITS_API = `${API_BASE}/tuits`;
+const TUITS_API = 'https://tuiter-node-server-app-3bsq.onrender.com/api';
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
     return response.data;
