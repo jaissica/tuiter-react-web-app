cee352a984b2e5715addf7f15ab8b93e7c16e98d
jaissica <jaissica.hora@gmail.com>
2023-06-13 19:13:18 -0400

    Revert

--- src/tuiter/profile/profile-screen.js
+++ src/tuiter/profile/profile-screen.js
@@ -29,7 +29,7 @@ console.log("profile" , profile)
      <h1>Profile Screen</h1>
      {profile && (<div>
        <div>
-        <label>First Name    </label>
+        <label>First Name</label>
         <input type="text" value={profile.firstName}
          onChange={(event) => {
           const newProfile = {
@@ -38,9 +38,8 @@ console.log("profile" , profile)
           setProfile(newProfile);
          }}/>
        </div>
-       <br/>
        <div>
-        <label>Last Name    </label>
+        <label>Last Name</label>
         <input type="text" value={profile.lastName}
          onChange={(event) => {
           const newProfile = {
@@ -50,8 +49,6 @@ console.log("profile" , profile)
          }}/>
        </div></div>
      )}
-     <br/>
-     <br/>
      <button
       onClick={() => {
         dispatch(logoutThunk());
